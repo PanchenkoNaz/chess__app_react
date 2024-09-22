c8a19d1c847c54c76afc2273b5909d5c7c74683d
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-09-22 17:06:26 +0200

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Preloader from './components/Preloader';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend'; // Backend для перетягування
+import Preloader from './components/Preloader'; 
 import ChessBoard from './components/ChessBoard';
-
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    const preloader = document.querySelector('.preloader-container');
     setTimeout(() => {
-      const preloader = document.querySelector('.preloader-container');
       if (preloader) {
-        preloader.classList.add('fade-out'); // Додаємо клас для плавного зникнення
+        preloader.classList.add('fade-out');
       }
       setTimeout(() => {
-        setLoading(false); // Показуємо основний контент після завершення анімації
-      }, 1000); // Затримка для завершення анімації
+        setLoading(false);
+      }, 1000); // Завершення анімації
     }, 3000); // Preloader відображається 3 секунди
   }, []);
 
   return (
     <div>
       {loading ? (
-        <Preloader /> // Показуємо preloader поки сторінка завантажується
+        <Preloader />
       ) : (
-        <ChessBoard /> // Показуємо шахову дошку після зникнення preloader-а
+        <DndProvider backend={HTML5Backend}> {/* Обертаємо у DndProvider */}
+          <ChessBoard />
+        </DndProvider>
       )}
     </div>
   );
