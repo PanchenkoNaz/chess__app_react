2d571fad4a44334d63f14e755e953e0356fca20b
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-10-02 16:42:17 +0200

    fix preloader

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,10 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import './Preloader.css'; // Підключаємо CSS для анімації
+import React, { useState, useEffect } from 'react';
+import './Preloader.css';
 
 const Preloader: React.FC = () => {
+  const [text, setText] = useState('');
+  const fullText = 'Chess Legacy'; // Текст, який зʼявлятиметься поступово
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText((prev) => prev + fullText[index]);
+      index++;
+      if (index === fullText.length) {
+        clearInterval(interval);
+      }
+    }, 250); // Затримка між буквами 150 мс
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="preloader-container">
-      <div className="preloader-text">Chess App</div>
+      <div className="preloader-text">
+        {text}
+      </div>
     </div>
   );
 };
